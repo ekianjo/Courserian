088d53ff730c5b76d3e55b5a321912b2dc732fa5
ekianjo <ekianjo@gmail.com>
2013-11-20 15:32:05 +0900

    Create main.py
    Started outline

---
+++ main.py
@@ -0,0 +1,21 @@
+#python
+#options when starting
+# Courses
+# update coursera-dl  -> check The Tube
+# register login/password
+#Pyzenity
+# Courses
+#  current registered courses
+#  add new one
+
+#  select courses
+#   names...
+
+#  download
+
+
+import PyZenity
+global course_list=[]
+global user_course_list=[]
+
+
