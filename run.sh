71daa137411b2f4e2126cb07b61f2f62c30874aa
ekianjo <ekianjo@gmail.com>
2014-01-17 14:48:10 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,14 @@
 
 #yad
 
-dialog=$(yad --center --title "Login Details" --form --field="Coursera Login" --field="password":H)
+yad --title="Courserian" --width=500  --button="gtk-ok:0"  --text="Welcome to Courserian. Courserian is a tool to make it easy for you to download, browse and manage your Coursera lessons."
 
+choice=$(yad --center --title="Courserian" --list --column="Menu" "Enter your Login details" "Add a course" "Remove/delete a course" "Browse your courses" "Download/update your courses")
+
+#LOGIN / PASSWORD
+dialog=$(yad --center --title="Login Details" --form --field="Coursera Login" --field="password":H)
 login=$(echo $dialog | awk 'BEGIN {FS="|" } { print $1 }')
 password=$(echo $dialog | awk 'BEGIN {FS="|" } { print $2 }')
 
-choice=$(yad --center --title "Courserian" --list --column="Menu" "Yo this is a first entry" "yo this is a second one")
+#ADDING A COURSE
+entry=$(yad --center --title="Courserian" --text="Enter the course code you wish to add. You can find this code or short name in the URL of your course. For example..." --form --field="Course Name")
