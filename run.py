d711b447ad4dd36fe87d1f7a6ef833b8c97b14f1
ekianjo <ekianjo@gmail.com>
2014-05-12 10:29:31 +0900

    Update run.py
    rewriting the whole script in Python... :)

--- run.py
+++ run.py
@@ -1,6 +1,33 @@
+import os, subprocess
 
 
 #yadcall
 
 def desktoporpandora():
-    pass
+    if os.path.isdir("/usr/pandora"):
+        runningmachine="pandora"
+        yadcall="./yad"
+        #locate smplayer
+    else:
+        runningmachine="desktop"
+        yadcall="yad"
+
+#quitprocedure
+
+def createcoursefolder():
+    if not os.path.isdir("COURSES"):
+        os.makedirs("COURSES")
+
+#see if i cannot simplify this function...
+def returncontents(directory):
+    contentdirs=[]
+    contentfiles=[]
+    for element in os.listdir(directory):
+        if os.path.isdir(element):
+            contentdirs.append(str(element))
+        else:
+            if os.path.isfile(element):
+                contentfiles.append(str(element))
+          
+
+#locate smplayer
